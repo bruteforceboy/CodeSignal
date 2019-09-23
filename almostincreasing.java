100f2b9f1acc5391d738b2bbea9fddabbb66197f
Ogbonna Chibuoyim <ogbonnachibuoyim12@gmail.com>
2019-09-23 15:09:13 +0100

    almostincreasing.java
    My solution to CodeSignal's question "Almost Increasing Sequence"

---
+++ almostincreasing.java
@@ -0,0 +1,28 @@
+/*
+ * Author: Ogbonna Chibuoyim
+ * Date Modified: 23-09-2019
+ */
+Boolean skip(int pos, int[] arr)
+{
+    for(int i=0;i<arr.length;i++)
+    {
+        if(i==pos)continue;
+        if(i==pos-1)
+        {
+            if((i+2)<arr.length&&arr[i]>=arr[i+2])
+                return false;
+        }
+        else
+        {
+            if((i+1)<arr.length&&arr[i]>=arr[i+1])
+                return false;
+        }
+    }
+    return true;
+}
+boolean almostIncreasingSequence(int[] sequence) {
+    for(int i=1;i<sequence.length;i++)
+        if(sequence[i]<=sequence[i-1]) // skip inbalance
+            return skip(i,sequence)||skip(i-1,sequence);
+    return true;
+}
