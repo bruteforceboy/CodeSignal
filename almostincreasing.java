522895203f08aa3446c5080abc63cc712f1ff0d3
Ogbonna Chibuoyim <ogbonnachibuoyim12@gmail.com>
2020-11-26 00:52:06 +0100

    Update almostincreasing.java

--- almostincreasing.java
+++ almostincreasing.java
@@ -2,28 +2,24 @@
  * Author: Ogbonna Chibuoyim
  * Date Modified: 23-09-2019-15:10
  */
-boolean skip(int pos, int[] arr)
-{
-    int len = arr.length;
-    for(int i=0;i<len;i++)
-    {
-        if(i==pos)continue;
-        if(i==pos-1)
-        {
-            if((i+2)<len&&arr[i]>=arr[i+2])
-                return false;
-        }
-        else
-        {
-            if((i+1)<len&&arr[i]>=arr[i+1])
-                return false;
-        }
+boolean skip(int pos, int[] arr) {
+  int len = arr.length;
+  for (int i = 0; i < len; i++) {
+    if (i == pos) continue;
+    if (i == pos - 1) {
+      if ((i + 2) < len && arr[i] >= arr[i + 2])
+        return false;
+    } else {
+      if ((i + 1) < len && arr[i] >= arr[i + 1])
+        return false;
     }
-    return true;
+  }
+  return true;
 }
+
 boolean almostIncreasingSequence(int[] sequence) {
-    for(int i=1;i<sequence.length;i++)
-        if(sequence[i]<=sequence[i-1]) // skip imbalance
-            return skip(i,sequence)||skip(i-1,sequence);
-    return true;
+  for (int i = 1; i < sequence.length; i++)
+    if (sequence[i] <= sequence[i - 1]) // skip imbalance
+      return skip(i, sequence) || skip(i - 1, sequence);
+  return true;
 }
