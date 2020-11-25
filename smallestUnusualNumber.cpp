77b0dd454b46f0952e128c9619de2b21fb1e0cb8
Ogbonna Chibuoyim <ogbonnachibuoyim12@gmail.com>
2020-11-26 00:51:04 +0100

    Update smallestUnusualNumber.cpp

--- smallestUnusualNumber.cpp
+++ smallestUnusualNumber.cpp
@@ -7,7 +7,7 @@ int smallestUnusualNumber(std::string a) {
   for (int i = 0; i < a.length(); i++) {
     sum += a[i] - '0';
     product *= a[i] - '0';
-    if (product > INT_MAX)return 10 - (a[a.length() - 1] - '0');
+    if (product > INT_MAX) return 10 - (a[a.length() - 1] - '0');
   }
   if (sum > product)return 0;
   return 10 - (a[a.length() - 1] - '0');
